449d4652de30681a8a7e3323266415e31f836d08
TassiloPitrasch <t.pitrasch@gmail.com>
2024-09-30 10:32:24 +0200

    Frontend changes accompanying commit 664b1624121a in the backend adding an enhanced project status

--- src/views/portfolio/projects/ProjectCreateProjectModal.vue
+++ src/views/portfolio/projects/ProjectCreateProjectModal.vue
@@ -369,7 +369,6 @@ export default {
           swidTagId: this.project.swidTagId,
           copyright: this.project.copyright,
           tags: tagsNode,
-          active: true,
           isLatest: this.project.isLatest,
         })
         .then((response) => {
@@ -432,7 +431,7 @@ export default {
     asyncFind: function (query) {
       if (query) {
         this.isLoading = true;
-        let url = `${this.$api.BASE_URL}/${this.$api.URL_PROJECT}?searchText=${query}&excludeInactive=true`;
+        let url = `${this.$api.BASE_URL}/${this.$api.URL_PROJECT}?searchText=${query}&enhancedStatus=IN_DEVELOPMENT&enhancedStatus=IN_PRODUCTION`;
         this.axios.get(url).then((response) => {
           if (response.data) {
             this.availableParents = response.data;
